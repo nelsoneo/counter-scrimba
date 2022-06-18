f7e288cb812625c1180a34dcafe3d442da5bf867
nelsoneo <nelsoneo@gmail.com>
2022-06-18 15:11:45 -0300

    more

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 let count = 0;
-let counterEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el")
 
 function increment(){
     count += 1;
-    counterEl.innerText = count;
+    countEl.innerText = count;
+}
+
+function save(){
+    console.log(count);
 }
